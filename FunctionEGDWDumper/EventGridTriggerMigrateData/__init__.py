b65ee3e7c54e75ab53c53556560d1202f36e7018
Yunhao Ling <adam_ling@outlook.com>
2019-03-06 18:43:49 -0800

    the functionality of python function app done

---
+++ FunctionEGDWDumper/EventGridTriggerMigrateData/__init__.py
@@ -0,0 +1,153 @@
+#----------------------------------------------------------------------------------------------
+# Follow the following two links to create a python function app in azure
+# https://docs.microsoft.com/en-us/azure/azure-functions/functions-create-first-function-python
+# https://docs.microsoft.com/en-us/azure/azure-functions/functions-reference-python
+#----------------------------------------------------------------------------------------------
+# Basic info:
+# 1. Azure SQL, SqlServer 2012
+# 2. ODBC Driver, 17 (The docker image is based on 17, but locally 13 is also compatible)
+# 3. pyodbc, 4.0.26
+#----------------------------------------------------------------------------------------------
+# TODO: 
+# 1. batch insert
+#----------------------------------------------------------------------------------------------
+# NOTE:
+# 1. bulk insert syntax of T-SQL isn't supported in sql server 2012
+# 2. executemany runs very slow on sql dataware house, on sql database runs normally
+# 3. the average sql insertion performs nearly 50 rows/s
+# 4. the concatenated sql statement string can't be too long, 500 insertion into one sql
+# string will fall.
+#----------------------------------------------------------------------------------------------
+#----------------------------------------------------------------------------------------------
+
+import json
+import logging
+import os
+import time
+from io import BytesIO
+from urllib.parse import urlparse
+
+import azure.functions as func
+from azure.storage import CloudStorageAccount
+import pyodbc
+from avro.datafile import DataFileReader
+from avro.io import DatumReader
+
+from ..SharedCode.windturbine_measure import WindTurbineMeasure 
+
+env_list = os.environ
+storage_connection_str = env_list["StorageConnectionString"]
+db_connection_str = env_list["PythonSqlDwConnection"]
+
+# parse the environment
+storage_infos = storage_connection_str.split(';')
+storage_account_name = storage_infos[1].split('=', 1)[1]
+storage_account_key = storage_infos[2].split('=', 1)[1]
+
+
+TABLE_NAME = "dbo.Fact_WindTurbineMetrics"
+
+BATCH_AMOUNT = 250  # an empirical number 
+
+# return container_name and blob_name
+def parse_file_url(file_url):
+
+    parse_obj = urlparse(file_url)
+    path = str(parse_obj.path)[1:]  # skip the first slash
+    arry = path.split("/", 1)
+
+    return arry[0], arry[1]
+    
+
+def dump(file_url):
+
+    # step1: download blob from storage
+    storage_account = CloudStorageAccount(storage_account_name, storage_account_key)
+
+    container_name, blob_name = parse_file_url(file_url)
+    blob_service = storage_account.create_block_blob_service()
+    blob = blob_service.get_blob_to_bytes(container_name, blob_name)
+    f = BytesIO(blob.content) # arvo file bytes data
+
+    reader = DataFileReader(f, DatumReader())
+
+    event_list = []
+    # step2: get the event data
+    for record in reader:
+        event_data = json.loads(record["Body"], encoding="ascii", object_hook=WindTurbineMeasure.obj_hook)
+        event_list.append(event_data)
+    
+    # step3: dump to the warehouse
+    batch_insert(event_list)
+
+
+def batch_insert(data_list):
+
+    db_connection = pyodbc.connect(db_connection_str, autocommit=False)
+    db_cursor = db_connection.cursor()
+
+    data_table = [(r.device_id, r.measture_time, r.generated_power, r.wind_speed, r.turbine_speed) for r in data_list]
+
+    start_time = time.time()
+
+    try:
+        sql, exec_cnt = "", 0
+
+        for data in data_table:
+            if exec_cnt < BATCH_AMOUNT:
+                sql += "INSERT INTO " + TABLE_NAME + " VALUES "
+                sql += ('(' + 'N\'' + data[0] + '\',N\'' + data[1] + '\',' + str(data[2]) + ',' + str(data[3]) + ',' + str(data[4]) + ');')
+                exec_cnt += 1
+            else:
+                sub_start_time = time.time()
+                logging.info('execute the sql')
+                db_cursor.execute(sql)
+
+                sub_end_time = time.time()
+                logging.info(str(sub_end_time - sub_start_time))
+                exec_cnt = 0
+                sql = ""
+
+        if exec_cnt != 0:
+            logging.info('final execute the sql')
+            db_cursor.execute(sql)
+
+        end_time = time.time()
+        logging.info(str(end_time-start_time))
+
+        #db_cursor.executemany("insert into " + TABLE_NAME + " (DeviceId, MeasureTime, GeneratedPower, WindSpeed, TurbineSpeed) values (?,?,?,?,?)", data_table)
+
+        logging.info("batch insert done")
+
+    except Exception as ex:
+        logging.error(ex)
+    finally:
+        db_cursor.close()
+        db_connection.commit()
+        db_connection.close()
+
+
+def main(event: func.EventGridEvent):
+    
+    logging.info("env info starts")
+    logging.info(db_connection_str)
+    logging.info(storage_account_name)
+    logging.info(storage_account_key)
+    logging.info("env info ends")
+
+    result = json.dumps({
+        'id': event.id,
+        'data': event.get_json(),
+        'topic': event.topic,
+        'subject': event.subject,
+        'event_type': event.event_type,
+    })
+
+    logging.info("received event:" + result)
+
+    try:
+        if event.get_json()["eventCount"] == 0:
+            pass
+        dump(event.get_json()["fileUrl"])
+    except Exception as e:
+        logging.error(e)
---
+++ FunctionEGDWDumper/SharedCode/windturbine_measure.py
@@ -0,0 +1,22 @@
+class WindTurbineMeasure:
+    def __init__(self, device_id, measture_time, generated_power, wind_speed, turbine_speed):
+        self.device_id = device_id
+        self.measture_time = measture_time
+        self.generated_power = generated_power
+        self.wind_speed = wind_speed
+        self.turbine_speed = turbine_speed
+
+    def to_dict(self):
+        return {
+            "DeviceId" : self.device_id,
+            "MeasureTime" : str(self.measture_time)[:-3],
+            "GeneratedPower" : self.generated_power,
+            "WindSpeed" : self.wind_speed,
+            "TurbineSpeed" : self.turbine_speed
+        }
+    
+    @staticmethod
+    def obj_hook(dic):
+        return WindTurbineMeasure(dic["DeviceId"], dic["MeasureTime"], dic["GeneratedPower"], dic["WindSpeed"], dic["TurbineSpeed"])
+        
+        
